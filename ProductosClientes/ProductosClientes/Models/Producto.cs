410b23f033d6acdb0d48706ac01663427acd7d3b
Valeria Tapia Fusco <valeria.tapia.fusco@gmail.com>
2019-03-09 10:31:03 -0400

    Entity framework instalado

---
+++ ProductosClientes/ProductosClientes/Models/Producto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductosClientes.Models
+{
+    public class Producto
+    {
+        int ProductoID;
+        string Nombre;
+        int Precio;
+    }
+}
